40cc873937f3efcbf50bf5ff53f76071e5aaa259
Yanis <59359557+yanis-man@users.noreply.github.com>
2021-05-13 23:10:29 +0200

    easier way to execute command

--- js/terminal/terminal.js
+++ js/terminal/terminal.js
@@ -1,11 +1,21 @@
 import Parser from './parser/commands.js'
+let PARSER = new Parser();
+
+function execute(brut_txt)
+{
+    let cmd_out = PARSER.cmd_parser(brut_txt);
+
+    let FINAL_STRING = "<div class=\"sub-cmd\"><div class=\"submited-cmd row\"><div class=\"prefix\"> yanis@buggy:</div>~$ "+ brut_txt +"</div><div class=\"cmd-return\">"+ cmd_out +"</div></div>"
+
+    return FINAL_STRING;
+}
+
 $(document).ready(function(){
     $('.cmd-input').keypress(function(event){
-        let PARSER = new Parser();
         var keycode = (event.keyCode ? event.keyCode : event.which);
         if(keycode == '13'){
-            let cmd_out = PARSER.cmd_parser($(this).val());
-           $(".terminal-out").append("<div class=\"sub-cmd\"><div class=\"submited-cmd row\"><div class=\"prefix\"> yanis@buggy:</div>~$ "+ $(this).val() +"</div><div class=\"cmd-return\">"+ cmd_out +"</div></div>");
+            let output = execute($(this).val())
+           $(".terminal-out").append(output);
            $(this).val('');
         }
     });
